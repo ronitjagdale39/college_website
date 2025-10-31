0f27e9c77134f6d2b16ee021b802f23abefa049f
ronitjagdale39 <ronit.jagdale.39@gmail.com>
2025-10-31 22:39:15 +0530

    Update settings.py-by chirayu durgude

--- college_website/college_website/settings.py
+++ college_website/college_website/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'web'
+    'web',
+    "django_icons",
 ]
 
 MIDDLEWARE = [
@@ -127,4 +128,9 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+DJANGO_ICONS = {
+    "ICONS": {
+        "fa": {"renderer": "fontawesome", "attrs": {"class": "fa-fw"}},
+    }
+}
 
